5ab3b742e11ce62cbd54069d45440dd93c90583c
jaros <judin.jaroslav@gmail.com>
2018-10-13 09:49:43 +0200

    extract component

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,51 +7,16 @@ import {
   Platform,
   ScrollView,
   StyleSheet,
-  Switch,
   Text,
   TouchableOpacity,
   TouchableHighlight,
   View,
   TextInput,
 } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
 import { WebBrowser } from 'expo';
 
+import TodoItem from '../components/TodoItem';
 import { MonoText } from '../components/StyledText';
-
-class TodoItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      done: false
-    };
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-      //console.log('update showDone', nextProps.showDone)
-  }
-
-  render() {
-    if (!this.state.done || this.props.showDone)
-      return (
-        <View style={{ flexDirection: 'row', alignItems: 'center'}}>
-          <Switch value={this.state.done} onValueChange={(value) => this.setState({done: value})} />
-          <Text style={styles.item}>{this.props.item.text}</Text>
-          <TouchableHighlight
-            onPress={() => {
-              console.log('delete a note', this.props.item.key);
-              this.props.onDelete(this.props.item.key);
-            }}
-            underlayColor="white">
-            <Ionicons name="ios-close-circle-outline" size={32} color="red" />
-          </TouchableHighlight>
-
-        </View>
-      )
-    else
-      return (<View/>)
-  }
-}
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -221,12 +186,6 @@ const styles = StyleSheet.create({
   },
   textInputContainer: {
     padding: 10,
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-    flexGrow: 1,
   },
   button: {
     marginBottom: 30,
